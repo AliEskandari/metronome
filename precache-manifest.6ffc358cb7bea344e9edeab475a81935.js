cea515c8d414c37aa29ef6e1517b8408fcd74297
Ali Eskandari <alieskandari3@gmail.com>
2020-12-05 20:56:04 -0800

    Updates

---
+++ precache-manifest.6ffc358cb7bea344e9edeab475a81935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8799a3de659488e4c6f442aee42bb1bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f15e0e19851180739f2a",
+    "url": "/static/css/main.6eb21a32.chunk.css"
+  },
+  {
+    "revision": "86dfab8c69137471eace",
+    "url": "/static/js/2.04144266.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.04144266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15e0e19851180739f2a",
+    "url": "/static/js/main.c3fec1e1.chunk.js"
+  },
+  {
+    "revision": "d4d6693d0d53aa3e8868",
+    "url": "/static/js/runtime-main.34c84b25.js"
+  },
+  {
+    "revision": "846642c754494f26387b0ff8d2874a36",
+    "url": "/static/media/clickSound.846642c7.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/metronome/precache-manifest.e85e0c1ad4ca92480d4feb48ca605671.js"
+  "/precache-manifest.6ffc358cb7bea344e9edeab475a81935.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/metronome/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
